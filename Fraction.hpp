a919ffebfb03efa30dbf9187f172fed3b72c5863
Field-Effect-Transistor <Field-Effect-Transistor@users.noreply.github.com>
2024-04-25 21:06:22 +0300

    last

--- Fraction.hpp
+++ Fraction.hpp
@@ -1,5 +1,6 @@
 #include "./Fraction/Fraction_.cpp"
 
+typedef dev::Fraction<short> ShortFraction;
 typedef dev::Fraction<int> Fraction;
 typedef dev::Fraction<long> LongFraction;
 typedef dev::Fraction<long long> LongLongFraction;
